958e4c6347a1c1998c17bfe6472cf1bc4d783a58
Samvardhan163 <samvardhan.gopathy@thoughtworks.com>
2022-06-10 12:50:31 +0530

    Initial Setup

---
+++ src/main/java/com/tw/todomatic/TodomaticApplication.java
@@ -0,0 +1,13 @@
+package com.tw.todomatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/todomatic/TodomaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.todomatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
